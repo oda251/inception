3eef05db16522ed77130f8d3d0d04d576fc68e3a
oda251 <pgwyaueu@gmail.com>
2025-05-06 14:14:58 +0900

    a

--- wordpress/init.sh
+++ wordpress/init.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-mkdir -p /run/php
-
-sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
-sed -i 's/;listen.allowed_clients = 127.0.0.1/listen.allowed_clients = 0.0.0.0/g' /etc/php/7.4/fpm/pool.d/www.conf
-
-mkdir -p /run/php
-
 echo "Waiting for MariaDB connection at ${WORDPRESS_DB_HOST}..."
 until mysqladmin ping -h"${WORDPRESS_DB_HOST}" -P3306 --silent; do
     echo "MariaDB is not available yet - sleeping for 2 seconds..."
@@ -14,12 +7,13 @@ until mysqladmin ping -h"${WORDPRESS_DB_HOST}" -P3306 --silent; do
 done
 echo "MariaDB is up."
 
-# if [ ! -f /var/www/html/wp-config.php ]; then
+if [ ! -f /var/www/html/wp-config.php ]; then
 
-    echo "Downloading WordPress..."
+	echo "WordPress is not installed. Proceeding with installation..."
+	echo "Downloading WordPress..."
 	wp core download --path=/var/www/html --locale=ja --allow-root
 
-    echo "Configuring WordPress..."
+  echo "Configuring WordPress..."
 	wp config create	--path=/var/www/html \
 						--allow-root \
 						--dbname=$WORDPRESS_DB_NAME \
@@ -27,8 +21,8 @@ echo "MariaDB is up."
 						--dbpass=$WORDPRESS_DB_PASSWORD \
 						--dbhost=$WORDPRESS_DB_HOST
 
-    echo "Installing WordPress..."
-	wp core install		--allow-root \
+	echo "Installing WordPress..."
+	wp core install	--allow-root \
 						--url=$WORDPRESS_URL \
 						--title=$WORDPRESS_TITLE \
 						--admin_user=$WORDPRESS_ADMIN \
@@ -37,17 +31,17 @@ echo "MariaDB is up."
 						--skip-email \
 						--path=/var/www/html/
 
-    echo "Creating WordPress user..."
-	wp user create		--allow-root \
+	echo "Creating WordPress user..."
+	wp user create	--allow-root \
 						$WORDPRESS_USER \
 						$WORDPRESS_USER_EMAIL \
 						--user_pass=$WORDPRESS_USER_PASSWORD \
 						--role=author \
 						--path=/var/www/html/
-# fi
 
-chown -R www-data:www-data /var/www/html
-chmod -R 755 /var/www/html
+else
+	echo "WordPress is already installed."
+fi
 
 echo "Starting PHP-FPM..."
 exec "$@"
